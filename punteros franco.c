f2619bbcb740312285c1761f640b1c4d6a1d3b81
francodauro <francodauro@gmail.com>
2016-11-15 16:22:10 -0300

    Subí a Github el archivo punteros franco.c

---
+++ punteros franco.c
@@ -0,0 +1,37 @@
+//Se define la directiva del compilador
+#include <stdio.h>
+
+//Se define programa principal
+int main (void)
+{
+
+//Se define un puntero llamado p1 que apunta a un número entero
+  int *p1;
+
+//Se asigna el número entero 5 a x  
+  int x=5;
+
+//Se asigna específicamente el número entero 4 al puntero p1
+  *p1=4;
+
+//Se da la orden de imprimir la dirección a la que apunta el puntero p1 en hexadecimal
+  printf ("La dirección almacenada en p1 es: %p\n", p1);
+
+//Se da la orden de imprimir el número entero 4 asignado a la dirección del puntero p1
+  printf ("Lo apuntado por p1: %d\n", *p1);
+
+//Se crea la relación entre el puntero p1 y el número entero 5 asignado a x  
+  p1=&x;
+
+//Se da la orden de imprimir la nueva dirección a la que apunta el puntero p1 en hexadecimal
+  printf ("La nueva dirección almacenada en p1 es: %p\n", p1);
+
+//Se da la orden de imprimir 
+  printf ("La dirección de x: %p\n", &x);
+
+//Se da la orden de imprimir el elemento al que apunta el puntero p1 que es el número entero 5
+  printf ("Lo apuntado por p1: %d\n", *p1);
+
+// Se da la orden a la función del programa principal devolver el valor 0  
+  return 0;
+}
